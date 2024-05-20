abdee860eadf0039a1c0d0628a2bc3c02f393b72
dany61955 <98032858+dany61955@users.noreply.github.com>
2024-05-21 03:05:20 +0530

    Update gfw_export_v3_1.py

--- gfw_export_v3_1.py
+++ gfw_export_v3_1.py
@@ -9,29 +9,38 @@ password = "your_password"
 # Disable warnings for unverified HTTPS requests (Not recommended for production)
 requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
-def get_tier0_gateways():
-    url = f"https://{nsx_ip}/policy/api/v1/infra/tier-0s"
+def get_gateways(gateway_type):
+    url = f"https://{nsx_ip}/policy/api/v1/infra/{gateway_type}"
     response = requests.get(url, auth=(username, password), verify=False)
     response.raise_for_status()  # Raise an exception for HTTP errors
-    tier0_gateways = response.json()
-    return [gateway['id'] for gateway in tier0_gateways['results']]
+    gateways = response.json()
+    return [gateway['id'] for gateway in gateways['results']]
 
-def get_gfw_rules(tier0_id):
-    url = f"https://{nsx_ip}/policy/api/v1/tier-0s/{tier0_id}/gateway-firewall"
+def get_gfw_rules(gateway_type, gateway_id):
+    url = f"https://{nsx_ip}/policy/api/v1/infra/{gateway_type}/{gateway_id}/gateway-firewall"
     response = requests.get(url, auth=(username, password), verify=False)
     response.raise_for_status()  # Raise an exception for HTTP errors
     return response.json()
 
 def main():
-    tier0_ids = get_tier0_gateways()
-    all_gfw_rules = {}
+    all_gfw_rules = {
+        "tier0": {},
+        "tier1": {}
+    }
     
+    tier0_ids = get_gateways('tier-0s')
     for tier0_id in tier0_ids:
-        gfw_rules = get_gfw_rules(tier0_id)
-        all_gfw_rules[tier0_id] = gfw_rules
+        gfw_rules = get_gfw_rules('tier-0s', tier0_id)
+        all_gfw_rules["tier0"][tier0_id] = gfw_rules
     
-    # Print or save the collected GFW rules
-    print(json.dumps(all_gfw_rules, indent=2))
+    tier1_ids = get_gateways('tier-1s')
+    for tier1_id in tier1_ids:
+        gfw_rules = get_gfw_rules('tier-1s', tier1_id)
+        all_gfw_rules["tier1"][tier1_id] = gfw_rules
+    
+    # Save the collected GFW rules to a file
+    with open('gfw_rules.json', 'w') as file:
+        json.dump(all_gfw_rules, file, indent=2)
 
 if __name__ == "__main__":
     main()
